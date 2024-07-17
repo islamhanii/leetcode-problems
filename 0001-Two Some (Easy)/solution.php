494cec3c8cd7b46eac8d408dc91d33c175312aeb
unknown <islamhani433@gmail.com>
2024-07-17 12:37:42 +0300

    Add 0001-Two Some (Easy) Problem

---
+++ 0001-Two Some (Easy)/solution.php
@@ -0,0 +1,21 @@
+<?php
+
+class Solution {
+    /**
+     * @param Int[] $nums
+     * @param Int $target
+     * @return Int[]
+     */
+    function twoSum($nums, $target) {
+        $length = count($nums);
+        $checkedNums = [];
+        for($i=0; $i<$length; $i++) {
+            if(in_array($nums[$i], $checkedNums)) continue;
+            array_push($checkedNums, $nums[$i]);
+            $index = array_search($target - $nums[$i], array_slice($nums, $i+1));
+            if($index !== false) {
+                return array($i, $index+$i+1);
+            }
+        }
+    }
+}
