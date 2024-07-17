377402e3bf9eb00584f01e51d71a87bf23c21195
unknown <islamhani433@gmail.com>
2024-07-17 13:00:19 +0300

    Update 0001-Two Some (Easy) Problem

--- 0001. Two Some (Easy)/solution.php
+++ 0001. Two Some (Easy)/solution.php
@@ -1,20 +1,22 @@
 <?php
 
-class Solution {
+class Solution
+{
     /**
      * @param Int[] $nums
      * @param Int $target
      * @return Int[]
      */
-    function twoSum($nums, $target) {
+    function twoSum($nums, $target)
+    {
         $length = count($nums);
         $checkedNums = [];
-        for($i=0; $i<$length; $i++) {
-            if(in_array($nums[$i], $checkedNums)) continue;
+        for ($i = 0; $i < $length; $i++) {
+            if (in_array($nums[$i], $checkedNums)) continue;
             array_push($checkedNums, $nums[$i]);
-            $index = array_search($target - $nums[$i], array_slice($nums, $i+1));
-            if($index !== false) {
-                return array($i, $index+$i+1);
+            $index = array_search($target - $nums[$i], array_slice($nums, $i + 1));
+            if ($index !== false) {
+                return array($i, $index + $i + 1);
             }
         }
     }
