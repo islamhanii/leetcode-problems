5bc409bf9a4b6efac8f37318ee0ea6a3301e171a
islamhanii <islamhani433@gmail.com>
2026-04-24 10:18:08 +0300

    Add SQL solutions for problems 0511, 1050, 1729, and 3793

---
+++ SQL/3793. Find Users with High Token Usage/solution.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    user_id,
+    COUNT(prompt) AS prompt_count,
+    ROUND(AVG(tokens), 2) AS avg_tokens
+FROM
+    prompts
+GROUP BY
+    user_id
+HAVING
+    prompt_count >= 3
+    AND MAX(tokens) > AVG(tokens)
+ORDER BY
+    avg_tokens DESC,
+    user_id;
